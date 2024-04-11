b7a6778cad884140a2c5b17decc8b6b7f274c5ad
bartlomiej-splesnialy <72617970+bartspl860@users.noreply.github.com>
2024-04-11 17:34:21 +0200

    project init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ippp4s4.quicksteel {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ippp4s4.quicksteel to javafx.fxml;
+    exports ippp4s4.quicksteel;
+}
