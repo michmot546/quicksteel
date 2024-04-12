438c141aa650d9fd9a84424bd3f6ab62a0d17bb5
bartlomiej-splesnialy <72617970+bartspl860@users.noreply.github.com>
2024-04-12 12:29:20 +0200

    Lepsze punkty

--- src/main/java/ippp4s4/quicksteel/MainController.java
+++ src/main/java/ippp4s4/quicksteel/MainController.java
@@ -1,15 +1,20 @@
 package ippp4s4.quicksteel;
 
+import javafx.beans.property.ObjectProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.ScatterChart;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -33,10 +38,13 @@ public class MainController implements Initializable {
     public Button generateBtn;
     @FXML
     public LineChart chart;
+    @FXML
+    public NumberAxis timeAxis;
+
     ArrayList<ArrayList<Double>> data = new ArrayList<>();
     File uploadedFile;
 
-    public void openFile(){
+    public void openFile() {
         FileChooser chooser = new FileChooser();
         chooser.setTitle("Open File");
         // Set extension filter
@@ -45,16 +53,16 @@ public class MainController implements Initializable {
         uploadedFile = chooser.showOpenDialog(new Stage());
     }
 
-    public void hideDTimeMenu(){
+    public void hideDTimeMenu() {
         var isOpen = dTime.isSelected();
         dTimeMenu.setDisable(!isOpen);
     }
 
     public void filterData() throws IOException {
-        if(uploadedFile == null || !uploadedFile.exists())
+        if (uploadedFile == null || !uploadedFile.exists())
             return;
         data.clear();
-        for(var vat = 0; vat < vatCount.getValue(); vat++){
+        for (var vat = 0; vat < vatCount.getValue(); vat++) {
             data.add(new ArrayList<>());
         }
         try {
@@ -63,21 +71,19 @@ public class MainController implements Initializable {
             while (myReader.hasNextLine()) {
                 List<String> line = Arrays.stream(myReader.nextLine().split(";")).toList();
                 i++;
-                if(i == 1 || i == 2)
+                if (i == 1 || i == 2)
                     continue;
                 line = line.subList(2, vatCount.getValue() + 2);
                 ArrayList<Double> numericDataLine = new ArrayList<>();
-                line.forEach(mes -> numericDataLine.add(Double.valueOf(mes.replace(',','.'))));
-                for(var vat = 0; vat < vatCount.getValue(); vat++){
+                line.forEach(mes -> numericDataLine.add(Double.valueOf(mes.replace(',', '.'))));
+                for (var vat = 0; vat < vatCount.getValue(); vat++) {
                     data.get(vat).add(numericDataLine.get(vat));
                 }
             }
             myReader.close();
-        }
-        catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("Data format error");
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
@@ -85,7 +91,7 @@ public class MainController implements Initializable {
         plotData();
     }
 
-    private void CalculateDimensionlessConcetration(){
+    private void CalculateDimensionlessConcetration() {
         int numThreads = data.size();
         ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
 
@@ -98,33 +104,75 @@ public class MainController implements Initializable {
         executorService.shutdown();
     }
 
-    private void processInnerArray(ArrayList<Double> dataArray){
+    private void processInnerArray(ArrayList<Double> dataArray) {
         var c0 = dataArray.get(0);
         var cinf = dataArray.get(dataArray.size() - 1);
-        for(var i = 0; i < dataArray.size(); i++){
+        for (var i = 0; i < dataArray.size(); i++) {
             var ct = dataArray.get(i);
             dataArray.set(i, (ct - c0) / (cinf - c0));
         }
     }
 
-    private void plotData(){
+    private void plotData() {
         chart.getData().clear();
-        for(var vat = 0; vat < vatCount.getValue(); vat++){
-            XYChart.Series series = new XYChart.Series();
+        createHelpSeries();
+
+        AtomicReference<BigDecimal> time = new AtomicReference<>(BigDecimal.ZERO);
+        for (var vat = 0; vat < vatCount.getValue(); vat++) {
+            Series series = new Series();
             series.setName("Kadź " + (vat + 1));
-            AtomicReference<BigDecimal> time = new AtomicReference<>(BigDecimal.ZERO);
+            time.set(BigDecimal.ZERO);
             data.get(vat).forEach(mes -> {
                 BigDecimal increment = new BigDecimal("0.3");
                 time.set(time.get().add(increment));
-                series.getData().add(new XYChart.Data(time.get(), mes));
+                var point = new Data<Number, Number>(time.get(), mes);
+                point.setNode(createSmallNode());
+                series.getData().add(point);
             });
             chart.getData().add(series);
         }
+
+        timeAxis.setUpperBound(Math.floor(time.get().doubleValue() + timeAxis.getTickUnit()));
+    }
+
+    private void createHelpSeries(){
+        Series series = new Series();
+        var point = new Data<>(-1, 0.8);
+        series.getData().addAll(point, new Data<>(200f, 0.8f));
+        series.setName("0.8");
+        chart.getData().add(series);
+
+        Series series2 = new Series();
+        var point2 = new Data<>(-1, 0.2);
+        series2.getData().addAll(point2, new Data<>(200f, 0.2f));
+        series2.setName("0.2");
+        chart.getData().add(series2);
+    }
+
+    private Node createSmallNode(){
+        var pane = new Pane();
+        pane.setShape(new Circle(2.0));
+        pane.setScaleShape(false);
+        return pane;
+    }
+
+    private Node createDataNode(ObjectProperty<Double> value) {
+        var label = new Label();
+        label.textProperty().bind(value.asString("%,.1f"));
+
+        var pane = new Pane(label);
+        pane.setShape(new Circle(6.0));
+        pane.setScaleShape(false);
+
+        label.translateXProperty().bind(label.widthProperty().divide(0.4));
+        label.translateYProperty().bind(label.heightProperty().divide(-1.3));
+
+        return pane;
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        vatCount.getItems().setAll(1,2,3,4,5,6);
+        vatCount.getItems().setAll(1, 2, 3, 4, 5, 6);
         vatCount.setValue(4);
     }
 }
